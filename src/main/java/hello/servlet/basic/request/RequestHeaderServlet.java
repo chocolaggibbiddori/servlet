115e3b0cda08e7aa6ccbfbe4d84ab9a26cb14719
chocolaggibbiddori <didrkd684@naver.com>
2022-11-09 15:29:12 +0900

    feat : (구)Http 헤더 정보 출력(request.getHeaderNames())

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
@@ -14,6 +15,7 @@ public class RequestHeaderServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         printStartLine(request);
+        printHeaders(request);
     }
 
     private static void printStartLine(HttpServletRequest request) {
@@ -26,6 +28,20 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getQueryString() = " + request.getQueryString());// username=hi
         System.out.println("request.isSecure() = " + request.isSecure()); // https 사용 유무
         System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+    //Header 모든 정보
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println("headerName = " + headerName);
+        }
+
+        System.out.println("--- Headers - end ---");
         System.out.println();
     }
 }
